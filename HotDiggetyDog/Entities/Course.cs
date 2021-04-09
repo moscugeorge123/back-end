929cc321dfaa59c4c88104344848e714d7911e75
Andrei <gandrei.raileanu@gmail.com>
2021-04-09 21:55:57 +0300

    dev

---
+++ HotDiggetyDog/Entities/Course.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotDiggetyDog.Entities
+{
+    public class Course
+    {
+        public Course()
+        {
+            this.Students = new HashSet<Student>();
+        }
+        public int CourseId{ get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public virtual ICollection<Student> Students { get; set; }
+    }
+}
---
+++ HotDiggetyDog/Entities/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotDiggetyDog.Entities
+{
+    public class Student
+    {
+        public Student()
+        {
+            this.Courses = new HashSet<Course>();
+        }
+        public int StudentId{ get; set; }
+        [Required]
+        public string Name{ get; set; }
+        public virtual ICollection<Course> Courses { get; set; }
+    }
+}
