465d8ef7ce1e6e2953c33dd15e09e120cb5b1212
Stefan-Stev <60571585+Stefan-Stev@users.noreply.github.com>
2021-05-18 00:28:12 +0300

    Role added
    Role added

--- AuthMicroservice/Models/AuthenticateResponse.cs
+++ AuthMicroservice/Models/AuthenticateResponse.cs
@@ -8,11 +8,14 @@ namespace HotDiggetyDog.Models
         public Guid Id { get; set; }
         public string Username { get; set; }
         public string Token { get; set; }
+
+        public string Role { get; set; }
         public AuthenticateResponse(User user, string token)
         {
             Id = user.Id;
             Username = user.Username;
             Token = token;
+            Role = user.Role;
         }
     }
 }
